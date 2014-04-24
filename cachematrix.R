92057c292fb3fe0d11832dfa229ba2923d728381
matvey.r87@gmail.com <matvey.r87@gmail.com>
2014-04-25 00:30:43 +0400

    R code to make Programming Assignment2 - cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions computes the inverse of matrix. Answer is stored in
+## cashe. In case of repeated computation of the same matrix, inverse answer is 
+## extracted from cache rather than computed again.
 
-## Write a short comment describing this function
+
+## 'makeCacheMatrix' converts original data matrix to specific list object
+## containing functions. They are then used inside 'cacheSolve' function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Empty variable to store the inverse of 'x'
+    inv_mx <- NULL
+    ## 'Re-writes' original data and resets cashed data (not used in current
+    ## implementation of 'casheSolve' function).
+    set <- function(y) {
+        x <<- y
+        inv_mx <<- NULL
+    }
+    
+    ## Defines data to be processed
+    get <- function() x
+    
+    ## Writes inverse matrix data to cache
+    setCacheMx <- function (inverse) inv_mx <<- inverse
+    
+    ## Extracts inverse matrix data from cache
+    getCacheMx <- function() inv_mx
+    
+    ## Creates list of 4 functions which is the output of the function
+    list(set = set, get = get,
+         setCacheMx = setCacheMx,
+         getCacheMx = getCacheMx)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' returns a matrix that is the inverse of 'x' and saves it 
+## into cache. If inverse of 'x' is calcalated repeatedly, extracts previously 
+## saved data from cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Gets inverse matrix from cache
+    inv_mx <-x$getCacheMx()
+    ## If inverse matrix already exists, extracts it from cache
+    if(!is.null(inv_mx)) {
+        message("getting cached data")
+        return(inv_mx)
+    }
+    ## Otherwise computes inverse of x from scratch 
+    data <- x$get()
+    inv_mx <- solve(data, ...)
+    x$setCacheMx(inv_mx)
+    return(inv_mx)
 }
